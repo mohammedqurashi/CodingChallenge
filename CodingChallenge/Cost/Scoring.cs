1a05c35cdd5b390452b1b54c1d5268f39e777677
mohammedqurashi <mohammedqurashi@MOHAMMED2871>
2016-06-21 13:39:30 +0530

    mohammed commit

--- CodingChallenge/Cost/Scoring.cs
+++ CodingChallenge/Cost/Scoring.cs
@@ -15,7 +15,7 @@ namespace Calculation
             double score = 0;
 
            
-                if (opn.RequestStartDate > res.AvlDate && !opn.MandotaroySkilss.Except(res.Skills).Any())  //&& res.Skills.Intersect(opn.MandotaroySkilss).Any())   //
+                if (opn.RequestStartDate > res.AvlDate && !opn.MandotaroySkilss.Except(res.Skills).Any())  
                 {
 
                     score = score + 1;
@@ -23,12 +23,14 @@ namespace Calculation
                     score = res.Rating == "A+" ? score + 0.2 : score;
                     score = res.Rating == "A" ? score + 0.1 : score;
                     score = opn.ProjectDomain!=null ? ( !res.DomainExperiance.Except(opn.ProjectDomain).Any() ? score + 0.2 : score) : score;
-                    //score = opn.ProjectDomain!=null ? (opn.ProjectDomain.Intersect(res.DomainExperiance).Any() ? score + 0.2 : score) : score;
                     score = res.YearsOfExperiance - opn.YearsOfExperiance > 0 ? Math.Max(0, score - (0.05 * (res.YearsOfExperiance - opn.YearsOfExperiance))) : score;
                     score = opn.IsKeyProject && opn.IsKeyPosition ? Math.Max(2, score) : score;
                     score = opn.IsKeyProject && !opn.IsKeyPosition ? Math.Max(1.3, score) : score;
                     score = !opn.IsKeyProject && opn.IsKeyPosition ? Math.Max(1.5, score) : score;
                     score = !opn.IsKeyProject && !opn.IsKeyPosition ? Math.Max(1, score) : score;
+
+                //TimeSpan ts = opn.AllocationEndDate - DateTime.Now;
+                //score = score - (ts.Days) * paneltyfactor;
                 }
           
 
